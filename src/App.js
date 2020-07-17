569cdf528105424ffdf708505525ecf9beb97c4c
Shaun Parry <shaun.parry@optum.com>
2020-07-17 17:36:33 +0100

    Tidy Up. General sketch out.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default () => (
+  <div className="App">
+    <h1>Paint Colour Selector</h1>
+    <label htmlFor="favcolor">Enter a Hex RGB for the colour</label>
+    <input type="text" id="favcolor" name="favcolor" />
+    <button>Select Colour</button>
+  </div>
+);
