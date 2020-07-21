ec64a0d9a52077172c60dd8abcf7afbe08443f2c
LaughingLemon <laughing.programmer@outlook.com>
2020-07-21 20:29:12 +0100

    Add paint colour sample

--- src/components/PaintSelection/index.js
+++ src/components/PaintSelection/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import PaintColour from "../../utils/PaintColour";
 import findNearestPaintColour from "../../utils/SelectColour";
+import "./style.css"
 
 export default (props) => {
     const paintColours = props.paintColours.map((colour) => new PaintColour(colour.name, parseInt(colour.hexCode.replace("#", "0x"), 16)));
 
-    console.log("props.value", props.value);
-
     const nearestColour = findNearestPaintColour(props.value, paintColours);
 
-    const backColour = "#" + nearestColour.hexCode.toString(16).padStart(6, '0'); 
-
-    const stylesObj = {
-        backgroundColor: backColour
-    };
-
-    console.log("stylesObj", stylesObj);
+    const innerColour = "#" + nearestColour.hexCode.toString(16).padStart(6, '0'); 
+    const outerColour = "#" + props.value.toString(16).padStart(6, '0'); 
 
     return (
         <div>
-            <div>{props.name}</div>
-            <div className={"box"} style={stylesObj}>&nbsp;</div>
-            <div>{nearestColour.name}</div>
-            <div>{nearestColour.hexCode.toString(16)}</div>
+            <div className={"colour-boxes box"}>
+                <div className={"outer-box"} style={{ backgroundColor: outerColour }}>
+                    <div className={"inner-box"} style={{ backgroundColor: innerColour }}>&nbsp;</div>
+                </div>
+            </div>
+            <div className={"box"}>
+                <div>{props.name}</div>
+                <div>{nearestColour.name}</div>
+                <div>{innerColour}</div>
+            </div>
         </div>
     );
 };
